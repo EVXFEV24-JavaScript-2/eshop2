8d8aa7e335cc92a043753cba88aa166f8f6ee0c1
EnanderW <williamenander@gmail.com>
2024-11-25 13:28:49 +0100

    create directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+/*
+
+Pages och routing
+Varukorg
+Mappstruktur
+Inlogg
+Generella komponenter
+Produktsida / visa produkter
+Söka produkter
+Specifik produkt sida
+Ladda in API data
+
+Pages komponenter:
+- Home
+- Shop
+- Featured products
+- Recommended products
+- Product view
+
+Generella komponenter:
+- Product cart
+- Product cart item
+- Product card
+- Navbar
+- Footer
+
+Klasser och funktioner för APIet:
+- Hämta produkter
+
+
+*/
+
 function App() {
   return <></>;
 }
